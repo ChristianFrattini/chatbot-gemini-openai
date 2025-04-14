45e5aede364ccd63579b8b214307e520ce6a2599
Christian Frattini <chrifrat1@gmail.com>
2025-04-14 12:24:14 +0100

    updated language model gemini2.0flash

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -4,6 +4,7 @@ import { initialMessage } from "@/lib/data";
 
 const google = createGoogleGenerativeAI({
   apiKey: process.env.GOOGLE_API_KEY || "",
+  //baseURL: "https://generativelanguage.googleapis.com/v1beta",
 });
 
 export const runtime = "edge";
@@ -27,7 +28,7 @@ export async function POST(request: Request) {
   const { messages } = await request.json();
 
   const stream = await streamText({
-    model: google("gemini-pro"),
+    model: google("gemini-2.0-flash"),
     messages: buildGoogleGenAIPrompt(messages),
     temperature: 0.4,
   });
